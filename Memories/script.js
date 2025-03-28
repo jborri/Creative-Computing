448a04f12645d6a6c0393f1a7b3c09ee4d9ffe75
jborri <josephborri@gmail.com>
2025-03-28 10:38:26 -0400

    music

--- Memories/script.js
+++ Memories/script.js
@@ -1,7 +1,58 @@
 /*
   This is your site JavaScript code - you can add interactivity!
 */
+function play() {
+  var audio = new Audio('A&W (Instrumental With Backing Vocals).mp3');
+  audio.play();
+  audio.loop = true;
+  audio.volume = 0.3;
+}
+function setCookie(c_name,value,exdays)
+{
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString()); + "; path=/"
+    document.cookie=c_name + "=" + c_value;
+}
 
+function getCookie(c_name)
+{
+    var i,x,y,ARRcookies=document.cookie.split(";");
+    for (i=0;i<ARRcookies.length;i++)
+    {
+      x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+      y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+      x=x.replace(/^\s+|\s+$/g,"");
+      if (x==c_name)
+        {
+        return unescape(y);
+        }
+      }
+}
+
+var song = document.getElementsByTagName('audio')[0];
+var played = false;
+var tillPlayed = getCookie('timePlayed');
+function update()
+{
+    if(!played){
+        if(tillPlayed){
+        song.currentTime = tillPlayed;
+        song.play();
+        played = true;
+        }
+        else {
+                song.play();
+                played = true;
+        }
+    }
+
+    else {
+    setCookie('timePlayed', song.currentTime);
+    }
+}
+setInterval(update,1000);
+sessionStorage.setItem('timePlayed', song.currentTime);
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 // console.log("Hello 🌎");
