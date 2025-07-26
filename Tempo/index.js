4dc77a249fb5ac4c8b3916835dc5717a09a6253e
jborri <josephborri@gmail.com>
2025-07-25 21:49:12 -0400

    thinking

---
+++ Tempo/index.js
@@ -0,0 +1,24 @@
+
+  const startButton = document.getElementById("titles");
+  const startMenu = document.getElementById("link_container");
+
+  startButton.addEventListener("click", () => {
+    // Toggle the visibility of the start menu
+    if (startMenu.classList.contains("hidden")) {
+      startMenu.classList.remove("hidden");
+      startMenu.style.display = "block";
+    } else {
+      startMenu.classList.add("hidden");
+      startMenu.style.display = "none";
+    }
+  
+  });
+
+  // Close the menu if clicked outside?
+  document.addEventListener("click", (event) => {
+    if (!startButton.contains(event.target) && !startMenu.contains(event.target)) {
+      startMenu.classList.add("hidden");
+      startMenu.style.display = "none";
+       console.log("did it work?")
+    }
+  });
